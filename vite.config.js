f25ef0c2050529a4a1e0f18c0fc6bf4dc5c5df8e
surion13 <samcardenas283@gmail.com>
2026-05-05 16:26:41 -0500

    ahora si

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-
+import {resolve} from "path"
 export default defineConfig({
   plugins: [tailwindcss()],
   build: {
